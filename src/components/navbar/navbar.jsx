f3eae8b0c83e9319cc5289f6becbf6f3c99e8a23
Aytacrzaeva <126054424+Aytacrzaeva@users.noreply.github.com>
2023-05-02 19:31:24 +0400

    first

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './navbar/navbar.css'
+
+function navbar() {
+  return (
+    <div>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Courses</a></li>
+            <li><a href="#">Programs</a></li>
+            <li><a href="#">Teachers</a></li>
+        </ul>
+    </nav>
+</div>
+  );
+}
+
+export default navbar
